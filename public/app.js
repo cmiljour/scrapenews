09339b324bc0e731f1898e462b789ef35cce11f7
cmiljour <cormil@gmail.com>
2017-09-19 11:20:25 -0500

    added popups and fixed delete comments display

--- public/app.js
+++ public/app.js
@@ -26,36 +26,39 @@
       method: "GET",
       url: "/scrape/"
     });
+    alert("Click on \"Display Articles\" to read!");
   });
 
   $(document).on("click", "#savedArticles", function() {
+    
     $.ajax({
       method: "GET",
       url: "/saved/"
     }).done(function(data){
       $("#articles").empty();
-        for (var i = 0; i < data.length; i++) {
-
-            if (data[i].comment == null){
-              console.log("it is null");
-              continue;
-            } else {
-              // Display the apropos information on the page
-              $("#articles").append(
-                `<br/>
-                <div class="jumbotron">
-                <p> ${data[i].title} 
-                <br/>
-                <a href='${data[i].link}'>${data[i].link}</a>
-                </p>
-                <div class="panel panel-default">
-                  <div class="panel-body">You Commented:  ${data[i].comment.body}</div>
-                </div>
-                <br/>
-                <div data-id=${data[i]._id} class="btn btn-danger scrape-new" id="delComment">Delete Comment</div>
-                </div>`);   
-            }
-        };
+      for (var i = 0; i < data.length; i++) {
+          // if document has a null comment field value, it stops displaying comments
+          // continue on to next record if null found
+          if (data[i].comment == null){
+            console.log("it is null");
+            continue;
+          } else {
+            // Display the apropos information on the page
+            $("#articles").append(
+              `<br/>
+              <div class="jumbotron">
+              <p> ${data[i].title} 
+              <br/>
+              <a href='${data[i].link}'>${data[i].link}</a>
+              </p>
+              <div class="panel panel-default">
+                <div class="panel-body">You Commented:  ${data[i].comment.body}</div>
+              </div>
+              <br/>
+              <div data-id=${data[i]._id} class="btn btn-danger scrape-new" id="delComment">Delete Comment</div>
+              </div>`);   
+          }
+      };
     });
   });
 
@@ -64,9 +67,6 @@
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
     var commentTxt = $("#" + thisId).val();
-    console.log(commentTxt);
-    
-  
     // Run a POST request to change the note, using what's entered in the inputs
     $.ajax({
       method: "POST",
@@ -80,11 +80,11 @@
       // With that done
       .done(function(data) {
         // Log the response
-        console.log("sent it to the DB");
         // Empty the articles section
         // $("#articles").empty();
       });
-  
+    
+    alert("Article Comment Saved!");
     // // Also, remove the values entered in the input and textarea for note entry
     // $("#titleinput").val("");
     // $("#bodyinput").val("");
@@ -99,4 +99,5 @@
         id: thisId
       }
     });
+    $(this).parent().remove();
   });
